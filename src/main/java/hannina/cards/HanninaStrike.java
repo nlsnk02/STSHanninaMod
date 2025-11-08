8ba1468a290006e645455d491c55e290323b44ec
nlsnk02 <=>
2025-11-08 22:29:31 +0800

    修改了最爱你了的描述，加上了切形态的腐化适配

--- src/main/java/hannina/cards/HanninaStrike.java
+++ src/main/java/hannina/cards/HanninaStrike.java
@@ -8,10 +8,12 @@ import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import hannina.fantasyCard.AbstractHanninaCard;
 import hannina.misc.SaveData;
 import hannina.utils.ChangePlayerModel;
+import hannina.utils.ModHelper;
 
 public class HanninaStrike extends AbstractHanninaCard implements CustomSavable<Integer> {
     public HanninaStrike() {
