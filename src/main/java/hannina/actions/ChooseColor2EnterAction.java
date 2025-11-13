e8094855c754a71e3c335f1b375536c4b63b1359
nlsnk02 <=>
2025-11-14 00:35:03 +0800

    尖塔之兽状态下不会弹出切形态的窗口了

--- src/main/java/hannina/actions/ChooseColor2EnterAction.java
+++ src/main/java/hannina/actions/ChooseColor2EnterAction.java
@@ -3,22 +3,28 @@ package hannina.actions;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.watcher.ChooseOneAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import hannina.fantasyCard.BlueColorCard;
 import hannina.fantasyCard.GreenColorCard;
 import hannina.fantasyCard.PurpleColorCard;
 import hannina.fantasyCard.RedColorCard;
+import hannina.powers.FusionPower;
 
 import java.util.ArrayList;
 
 public class ChooseColor2EnterAction extends AbstractGameAction {
     @Override
     public void update() {
-        ArrayList<AbstractCard> stanceChoices = new ArrayList<>();
-        stanceChoices.add(new RedColorCard());
-        stanceChoices.add(new GreenColorCard());
-        stanceChoices.add(new BlueColorCard());
-        stanceChoices.add(new PurpleColorCard());
-        addToTop(new ChooseOneAction(stanceChoices));
+
+        if(!AbstractDungeon.player.hasPower(FusionPower.POWER_ID)) {
+            ArrayList<AbstractCard> stanceChoices = new ArrayList<>();
+            stanceChoices.add(new RedColorCard());
+            stanceChoices.add(new GreenColorCard());
+            stanceChoices.add(new BlueColorCard());
+            stanceChoices.add(new PurpleColorCard());
+            addToTop(new ChooseOneAction(stanceChoices));
+        }
+
         this.isDone = true;
     }
 }
